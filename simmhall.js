8d59b2591c754e7ce3d698e19c5b707f2e8a8318
olbo98 <ob655088@gmail.com>
2016-02-11 11:48:02 +0100

    Särade på kid[i].spawn och kid[i].paint

--- simmhall.js
+++ simmhall.js
@@ -1,17 +1,16 @@
-function barn(img, smutsig) {
+function Barn(img) {
 
     this.x = Math.random() * (1000 - 300) + 300;
     this.y = Math.random() * (800 - 200) + 200;
     this.img = img;
-    this.smutsig = smutsig;
     this.ren = 1;
-    this.vx = Math.random() * 4;
+    this.vx = 0;
     this.vy = 0;
 
 }
 
 
-barn.prototype.spawn = function () {
+Barn.prototype.spawn = function () {
 
     this.x = Math.random() * (1000 - 300) + 300;
     this.y = Math.random() * (800 - 200) + 200;
@@ -19,8 +18,15 @@ barn.prototype.spawn = function () {
 
 }
 
-barn.prototype.paint = function (ctx) {
+Barn.prototype.paint = function (ctx) {
 
     ctx.drawImage(this.img, this.x, this.y, 100, 100);
 
+}
+
+Barn.prototype.move = function(){
+    
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy
+    
 }
